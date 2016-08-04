e8b0407d1fbfd498887ffd620b6bac362412042c
rouz majlessi <rmajlessi@gmail.com>
2016-08-03 21:38:28 -0700

    edeit route in homemodule also create a new view file and add a form

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,13 +12,10 @@ namespace HairSalon
         List<Stylist> allSytlists = Stylist.GetAll();
         return View["index.cshtml"];
       };
-
-      Post["/stylist/new"] = _ => {
-          Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
-          newStylist.Save();
-          List<Stylist> allStylists = Stylist.GetAll();
-          return View["index.cshtml", allStylists];
-        };
+      Get["/clients"] = _ => {
+        List<Client> AllClients = Client.GetAll();
+        return View["clients.cshtml", AllClients];
+      };
     }
 
   }
